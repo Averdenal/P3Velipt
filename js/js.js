d70e455b0f6a99a752a77092af43c014857a1a70
Averndenal <amaury.y.verdenal@gmail.com>
2019-12-10 15:56:49 +0100

    maps

--- js/js.js
+++ js/js.js
@@ -5,6 +5,73 @@ var dataSlider =
     {"textInfo":"Demo3", "URL":"imgs/sliders/slider1jpg.jpg"}
 ]
 
+/* https://docs.mapbox.com/ */
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW1hdXJ5diIsImEiOiJjazN6d2dybDAwODF3M21tdHl1aHVucnU5In0.wxEqA0_vYEl5O4kiOnSXHw';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v9',
+center: [-1.553621, 47.218371],
+zoom: 12
+});
+
+map.on("load", function () {
+    /* Image: An image is loaded and added to the map. */
+    map.loadImage("https://i.imgur.com/MK4NUzI.png", function(error, image) {
+        if (error) throw error;
+        map.addImage("custom-marker", image);
+        /* Style layer: A style layer ties together the source and image and specifies how they are displayed on the map. */
+        map.addLayer({
+            id: "markers1",
+            type: "symbol",
+            source: {
+            type: "geojson",
+            data: {
+            type: 'FeatureCollection',
+            features: [
+            {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+            type: "Point",
+            coordinates: [-1.5465, 47.2159]
+            }
+            }
+            ]
+            }
+            },
+            layout: {
+                "icon-image": "custom-marker",
+            }
+            });
+        map.addLayer({
+            id: "markers",
+            type: "symbol",
+            /* Source: A data source specifies the geographic coordinate where the image marker gets placed. */
+            source: {
+                type: "geojson",
+                data: {
+                    type: 'FeatureCollection',
+                    features: [
+                        {
+                        type: 'Feature',
+                        properties: {},
+                        geometry: {
+                            type: "Point",
+                            coordinates: [-1.558, 47.2152]
+                            }
+                        }
+                    ]
+                }
+            },
+            layout: {
+            "icon-image": "custom-marker",
+            }
+        });
+    });
+});
+
+
 class carousel{
     /**
      * @param  {HTMLElement} element
@@ -55,5 +122,7 @@ document.addEventListener("DOMContentLoaded", function(){
         slidersToScroll: 3,
         sliderVisible:3
     },dataSlider)
+    
 })
+
 
