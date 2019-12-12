459068d669786d71ddaf460c6e95bef0324fcc4e
Averndenal <amaury.y.verdenal@gmail.com>
2019-12-12 15:13:01 +0100

    génération div

--- js/data.js
+++ js/data.js
@@ -2,19 +2,58 @@ let URL = "https://api.jcdecaux.com/vls/v1/stations?contract=nantes&apiKey=09698
 var jqxhr = $.getJSON( URL, function() {
     console.log( "success" );
   })
-    .done(function() {
-      console.log( "second success" );
-    })
-    .fail(function() {
-      console.log( "error" );
-    })
-    .always(function() {
-      console.log( "complete" );
+  .always(function() {
+    jqxhr.responseJSON.forEach(element => {
+      console.log( element );
+      var station = new Station(element.name, element.address, element.position, element.status, element.available_bikes, element.bike_stands)
+      console.log (station.name)
+      $info.appendChild(station.HTML_Contruction())
+
     });
-   
-  // Perform other work here ...
-   
-  // Set another completion function for the request above
-  jqxhr.always(function() {
-    console.log( jqxhr.responseJSON );
   });
+  
+  class Station{
+    
+    constructor(name,adresse,position,status, veloDispo, maxplaces){
+      this.name = this.nameChange(name)
+      this.adresse = adresse
+      this.position = position
+      this.status = status
+      this.veloDispo = veloDispo
+      this.maxplaces = maxplaces
+    }
+
+    /**
+     * retire les infos nombre du nom
+     * @param {string} name 
+     */
+    nameChange(name){
+      if(name.substr(0,1) ==='#'){
+        name = name.substr(7)
+        if (name.substr(0, 1) === "-"){
+          name = name.substr(1)
+        }
+      }
+      return name
+    }
+
+    HTML_Contruction(){
+      var div = document.createElement("div")
+      div.setAttribute('class','js-item')
+      div.setAttribute('data-lat',this.position.lat)
+      div.setAttribute('data-lng',this.position.lng)
+      div.setAttribute('data-dispo',this.veloDispo)
+      div.setAttribute('data-maxplaces',this.maxplaces)
+      var p = document.createElement('h3')
+      p.textContent = this.name
+      div.appendChild(p)
+      return div
+    }
+  }
+
+
+  let $info = document.querySelector('#info')
+
+  if ($info !== null){
+    console.log("present")
+  }
