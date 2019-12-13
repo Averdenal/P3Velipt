0de7cd5c7f34382b33df9853a6e3252bbe542e3b
averdenal <amaury.y.verdenal@gmail.com>
2019-12-13 13:43:56 +0100

    correct

--- js/class.js
+++ js/class.js
@@ -1,34 +1,42 @@
-let URL = "https://api.jcdecaux.com/vls/v1/stations?contract=nantes&apiKey=096983f1e05f95e3c792a500e2499e840cf58f2d"
-var $info = document.querySelector("#info")
-/**
- * initialisation de la map
- */
-const initMap = async function(){
-  let map = new leafletMaps()
-  await map.laod($map) //await - attentre de bon chargement de la map avant de continuer 
-
-  var jqxhr = $.getJSON( URL, function() {
-    console.log( "success" );
-})
-.always(() => {
-
-        jqxhr.responseJSON.forEach(element => {
-            var station = new Station(element.number,element.name, element.address, element.position, element.status, element.available_bikes, element.bike_stands)
-            $info.appendChild(station.HTML_Contruction())
-            console.log("A: "+ element.position.lat + "B:" + element.position.lng)
-          map.addMarket(element.position.lat,element.position.lng,element.name)
-        });
-
-    });
+class leafletMaps{
+    constructor(){
+        this.map = null
+        this.bounds = []
+    }
+    /**
+     * chargement de la map
+     * @param {HTML_Element} element 
+     */
+    async laod(element){
+        return new Promise((resolve,reject) => {
+            this.map = L.map(element)
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+            {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(this.map)
+            resolve()
+        })
+    }
+    addMarket(lat,lng,info){
+      let point = [lat,lng]
+      this.bounds.push(point)
+        L.popup({
+            closeButton:false,
+            autoClose:false,
+            closeOnEscapeKey:false,
+            closeOnClick:false,
+            className:'popclass'
+        })
+        .setLatLng(point)
+        .setContent(info)
+        .openOn(this.map);
+    }
+    centre(){
+      this.map.fitBounds(this.bounds)
+    }
 }
-let $map = document.querySelector('#map')
-if($map !== null){
-  initMap()
-}
 
-
-
-  class Station{
+class Station{
 
     constructor(number,name,adresse,position,status, veloDispo, maxPlaces){
       this.number = number
@@ -71,19 +79,10 @@ if($map !== null){
     HTML_Contruction(){
       var div = document.createElement("div")
       div.setAttribute('class','js-item')
-      div.setAttribute('data-lat',this.position.lat)
-      div.setAttribute('data-lng',this.position.lng)
-      div.setAttribute('data-dispo',this.veloDispo)
-      div.setAttribute('data-maxplaces',this.maxPlaces)
 
       var h3 = document.createElement('h3')
       h3.textContent = this.name
       div.appendChild(h3)
-      
-      /*var p = document.createElement('p')
-      p.textContent = "Station n° " +this.number
-      p.setAttribute('class','station')
-      div.appendChild(p)*/
 
       var adresse = document.createElement('p')
       adresse.textContent = "Adresse: " +this.adresse
---
+++ js/fonctions.js
@@ -0,0 +1,29 @@
+let URL = "https://api.jcdecaux.com/vls/v1/stations?contract=nantes&apiKey=096983f1e05f95e3c792a500e2499e840cf58f2d"
+var $info = document.querySelector("#info")
+/**
+ * initialisation de la map
+ */
+const initMap = async function(){
+  let map = new leafletMaps()
+  await map.laod($map) //await - attentre de bon chargement de la map avant de continuer 
+  
+
+  var jqxhr = $.getJSON( URL, function() {
+    console.log( "success" );
+})
+.always(() => {
+
+        jqxhr.responseJSON.forEach(element => {
+            var station = new Station(element.number,element.name, element.address, element.position, element.status, element.available_bikes, element.bike_stands)
+            $info.appendChild(station.HTML_Contruction())
+            console.log("A: "+ element.position.lat + "B:" + element.position.lng)
+          map.addMarket(element.position.lat,element.position.lng,element.name)
+        });
+        map.centre()
+
+    });
+}
+let $map = document.querySelector('#map')
+if($map !== null){
+  initMap()
+}
